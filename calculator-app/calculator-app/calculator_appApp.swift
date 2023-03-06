816822171290e480cad9f19e45420d2a4870942e
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-06 11:03:53 -0800

    created calculator app project

---
+++ calculator-app/calculator-app/calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appApp.swift
+//  calculator-app
+//
+//  Created by Jonathan Bigbee on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
